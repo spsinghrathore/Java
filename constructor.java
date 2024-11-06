ebb367c515d6aaa366d170cd4a7f64f4e83ab3a9
SP <124254399+spsinghrathore@users.noreply.github.com>
2024-11-06 09:49:10 +0530

    Update constructor.java
    with two more constructor

--- constructor.java
+++ constructor.java
@@ -1,10 +1,21 @@
 class Constructor {
     String vidName;
-    String duration;
+    int duration;
     String size;
 
     public Constructor(String vidName){
         this.vidName = vidName;
+
+    }
+     public Constructor(String vidName, int duration){
+        this.vidName = vidName;
+        this.duration = duration;
+
+    }
+     public Constructor(String vidName, int duration,String size){
+        this.vidName = vidName;
+        this.duration = duration;
+        this.size = size;
 
     }
 
