7134c94544301a6a65ce449d4058f612a55ec2ca
SP <spsinghrathore1807@gmail.com>
2024-11-06 09:55:04 +0530

    added main method

--- constructor.java
+++ constructor.java
@@ -1,22 +1,33 @@
-class Constructor {
+class constructor {
     String vidName;
     int duration;
     String size;
 
-    public Constructor(String vidName){
+    public constructor(String vidName){
         this.vidName = vidName;
 
     }
-     public Constructor(String vidName, int duration){
+     public constructor(String vidName, int duration){
         this.vidName = vidName;
         this.duration = duration;
 
     }
-     public Constructor(String vidName, int duration,String size){
+     public constructor(String vidName, int duration,String size){
         this.vidName = vidName;
         this.duration = duration;
         this.size = size;
 
+    }
+
+    public void detail(){
+        System.out.println("Title: " + vidName);
+        System.out.println("Duration: " + duration);
+        System.out.println("Size: " + size);
+    }
+
+    public static void main(String[] args){
+        constructor vid1 = new constructor("Java in 18 minutes",18,"180mb");
+        vid1.detail();
     }
 
 }
